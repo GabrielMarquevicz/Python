58d63a5a259a1e4a473c674a3e7fc6ac6ab88736
GabrielMarquevicz <126807737+GabrielMarquevicz@users.noreply.github.com>
2023-06-09 16:18:11 -0300

    Update python.py

--- python.py
+++ python.py
@@ -1,8 +1,8 @@
 # Python
 
-def fib(n):
-  a, b = 0, 1
-  while a < n:
-    print(a, end= ' ')
-    a, b = b, a + b
-fib(int(input("Digite o  número pra fazer fibonacci: ")))
+def fib(n): # def= criação de uma função com nome logo em seguida
+  a, b = 0, 1 # definição das variáveis
+  while a < n: # while = loop enquanto não chegar o resultado pedido
+    print(a, end= ' ') # print = imprimir o resultado e o end = vai fazer com que o resultado apareça no final da operação
+    a, b = b, a + b 
+fib(int(input("Digite o  número pra fazer fibonacci: "))) # input = ao valor que "n" vai receber, como se fosse scanf do C
