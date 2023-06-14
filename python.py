1cca7aa7a625ac4b8bf444a96c1d48c2ce419ec9
GabrielMarquevicz <126807737+GabrielMarquevicz@users.noreply.github.com>
2023-06-14 19:23:31 -0300

    Update python.py

--- python.py
+++ python.py
@@ -1,8 +1,33 @@
 # Python
 
-def fib(n): # def= criação de uma função com nome logo em seguida
-  a, b = 0, 1 # definição das variáveis
-  while a < n: # while = loop enquanto não chegar o resultado pedido
-    print(a, end= ' ') # print = imprimir o resultado e o end = vai fazer com que o resultado apareça no final da operação
-    a, b = b, a + b 
-fib(int(input("Digite o  número pra fazer fibonacci: "))) # input = ao valor que "n" vai receber, como se fosse scanf do C
+import cv2
+import numpy as np
+
+
+     
+
+img = cv2.imread('j.png',0)
+img_opening = cv2.imread('j_ruido.png',0)
+img_closing = cv2.imread('j_furos.png',0)
+altura, largura = img.shape
+kernel = np.ones((5,5),np.uint8)
+print(kernel)
+     
+
+erosion = cv2.erode(img,kernel,iterations = 2)
+dilation = cv2.dilate(img,kernel,iterations = 2)
+     
+
+gradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
+opening = cv2.morphologyEx(img_opening, cv2.MORPH_OPEN, kernel)
+closing = cv2.morphologyEx(img_closing, cv2.MORPH_CLOSE, kernel)
+     
+
+
+#Caso usa com Python no próprio computador
+cv2.imshow('in', img)
+cv2.imshow('erosion_out', erosion)
+cv2.imshow('dilation_out', dilation)
+cv2.imshow('opening_out', opening)
+cv2.imshow('closing_out', closing)
+cv2.imshow('gradient_out', gradient)
